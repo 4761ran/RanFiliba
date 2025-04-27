e9ca2394222e129213a2ee616b2b05388d0394c1
4761ran <filiba.ran@gmail.com>
2025-04-27 17:56:24 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //creating the object
+
+        MobilePhone p = new MobilePhone("Apple","iphoneX",4500);
+        MobilePhone p2 = new MobilePhone("Xioami","Redmi Note 11",1120);
+        System.out.println("Phone 1:");
+        p.printDetails();
+        System.out.println("Phone 2:");
+        p2.printDetails();
+
+        System.out.println("After Discount:");
+        p.discount();
+        p2.discount();
+        p.printDetails();
+        p2.printDetails();
+
+
+    }
+}
---
+++ MobilePhone.java
@@ -0,0 +1,23 @@
+public class MobilePhone {
+    
+    private String brand;
+    private String model;
+    private double price;
+    //Constructor
+    public MobilePhone (String brand, String model, double price){
+        this.brand=brand;
+        this.model=model;
+        this.price=price;
+    }
+    //printing the details
+    void printDetails(){
+        System.out.println("The brand is: " + this.brand);
+        System.out.println("The model is:" + this.model);
+        System.out.println("The price is:" + this.price);
+
+    }
+
+    void discount(){
+        this.price=0.9*this.price;
+    }
+}
